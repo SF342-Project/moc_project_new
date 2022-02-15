5f684c25c146a6aa4fdb6ba0acd3af98232416df
OldSchool Dev <fam.b.boy2@gmail.com>
2022-02-15 10:47:17 +0700

    update routes/prices.js

--- backend/routes/Prices.js
+++ backend/routes/Prices.js
@@ -3,13 +3,20 @@ const Price = require('../models/Prices')
 const router = express.Router()
 const fetch = require('node-fetch');
 
-function dateFormat(dt){
+function reqDateFormat(dt){
     const d = new Date(dt)
     const year = d.getFullYear();
     const month = d.getMonth()+1;
     const date = d.getDate();
-    return year + '-' + month + '-' + date
+    var localTime = year + '-' + month.toString().padStart(2, '0') + '-' + date
+    // return localTime + "T00:00:00";
+    return localTime;
 }
+
+function dbDateFormat(localTime){
+    return localTime + "T00:00:00";
+}
+
 
 function subDays(date, days){
     const result = new Date();
@@ -26,27 +33,58 @@ function getProductPrices(product_id,from_date,to_date){
     return fetchData
 }
 // https://stackoverflow.com/questions/12467102/how-to-get-the-latest-and-oldest-record-in-mongoose-js-or-just-the-timespan-bet/54741405
+
+
 router.get('/now/:id',async (req,res) =>{
-    var filtered = await Price.find({'id':req.params.id,})
     var now = new Date()
-    var now_formatted = dateFormat(now)
-    var before = subDays(now,1)
-    var before_formatted = dateFormat(before)
-    // console.log(filtered.length === 0);
-    // var now0 = new Date(now.setHours(24,0,0,0));
-    // console.log(now0);
-    var product_detail = await getProductPrices(req.params.id,before_formatted,now_formatted)
-    for(var i = 0; i< product_detail.length; i++){
-        // console.log(product_detail[i]);
-        var prodt = product_detail[i]
-        // var _date = dateFormat(prodt.date);
-        var _date = prodt.date
-        var price_min = prodt.price_min;
-        var price_max = prodt.price_max;
-        console.log(_date,price_min,price_max);
+    var now_formatted = reqDateFormat(now)
+    var before = subDays(now,15)
+    var before_formatted = reqDateFormat(before)
+    
+    var now_formatted_db = dbDateFormat(now_formatted)
+    var filtered = await Price.find({'id':req.params.id,'date':now_formatted_db})
+
+    if (filtered.length === 0){
+        var product_detail = await getProductPrices(req.params.id,before_formatted,now_formatted)
+        for(var i = 0; i< product_detail.length; i++){
+            var prodt = product_detail[i]
+            var _date = prodt.date
+            var price_min = prodt.price_min;
+            var price_max = prodt.price_max;
+            var findData = await Price.find({'id':req.params.id,'date':_date})
+            if (findData.length === 0){
+                var newPrices = new Price({
+                    "id":req.params.id,
+                    "date": _date,
+                    "price_min":price_min,
+                    "price_max":price_max
+                })
+                await newPrices.save()
+                console.log(newPrices);
+            }
+            // else{
+            //     console.log('Data is exits');
+            // }
+        }
+        
+        var latestData = await Price.findOne({'id':req.params.id}, {}, { sort: { 'date' : -1 } });
+        res.send(latestData)
 
     }
-    
+    else{
+        res.send(filtered)
+    }
+
 })
+
+router.get('/compare/:id/:from/:to',async (req,res) =>{
+    var from_date_db = dbDateFormat(req.params.from)
+    var to_date_db = dbDateFormat(req.params.to)
+    
+    var filtered = await Price.find({'id':req.params.id,'date':{"$gte": from_date_db, "$lt": to_date_db}})
+    res.send(filtered)
+})
+
+
 
 module.exports = router
