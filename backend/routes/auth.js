12f3b3c977e22527370b38c1715fde9f89c79fb6
Mansea <6210742034@student.tu.ac.th>
2022-03-12 23:21:04 +0700

    Implement hashing password

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('../models/Users');
+const bcrypt = require('bcryptjs')
 const {registerValidation} = require('../validation');
 
 router.post('/register', async (req, res) => {
@@ -12,10 +13,14 @@ router.post('/register', async (req, res) => {
   const emailExist = await User.findOne({email: req.body.email});
   if(emailExist) return res.status(400).send('Email already exists!!')
 
+  //Hash passwords
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(req.body.password, salt);
+
   const user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password,
+    password: hashPassword,
   });
 
   try {
